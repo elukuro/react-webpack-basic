464eb6ba78a6ea94365c180766cd9d411cb989e3
elukuro <heruhartantot@gmail.com>
2017-06-03 14:19:07 +0700

    first init

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const css=require('./app.scss');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+	<h1>hello word try react js with webpack with own setup</h1>,
+	document.getElementById('root')
+);
+
