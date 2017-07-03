c38834c095b73b39b6241951681962c61f31e8e5
heru <heru.hartanto@beritagar.id>
2017-07-03 15:27:04 +0700

    shopping list

--- src/container/homina.js
+++ src/container/homina.js
@@ -24,18 +24,54 @@ class Homina extends Component{
 		console.log(this.state.buyItems[0])
 	}
 
+	addItem(e){
+		e.preventDefault();
+		const {buyItems}=this.state;
+		const newItem=this.newItem.value;
+
+		this.setState({
+			buyItems:[...this.state.buyItems, newItem]
+		})
+		this.addForm.reset();
+
+	}
+
 	render(){
 		return(
 			<div>
-				<h1>homina {this.state.title}</h1>
-				<Greeting name="test" age="18" /><br/>
-				{
-					this.state.buyItems.map(item=>{
-						return(
-							<p key={item}>{item}</p>
-						)
-					})
-				}
+				<div className="row">
+					<form ref={(input)=>this.addForm=input} className="form-inline" onSubmit={(event)=>{this.addItem(event)}}>
+						<div className="form-group">
+							<input ref={(input)=>this.newItem=input} type="text" className="form-control"/>
+						</div>
+						<button type="submit" className="btn btn-primary">add</button>
+					</form>
+				</div>
+				<div className="row">
+					<div className="col-md-6">
+						<Greeting name="test" age="18" /><br/>
+						 <table className="table">
+						    <thead>
+						      <tr>
+						        <th>item</th>
+						        <th>action</th>
+						      </tr>
+						    </thead>
+						    <tbody>
+						   		 	{
+										this.state.buyItems.map(item=>{
+											return(
+												<tr key={item}>
+													<td>{item}</td>
+													<td width="20px"><button className="btn btn-default">delete</button></td>
+												</tr>
+											)
+										})
+									} 
+						    </tbody>
+						  </table>
+				  	</div>
+				</div>
 			</div>
 		)
 	}
